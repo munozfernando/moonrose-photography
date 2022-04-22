ac3e9250413e8eddf1bf84dd72c8993578d4042d
Fernando Munoz <munofern@isu.edu>
2022-04-22 17:42:49 -0600

    testing tracked branch

--- pages/showcase/all.js
+++ pages/showcase/all.js
@@ -3,18 +3,18 @@ import Layout from "../../components/layout"
 export default function All() {
     return (
         <section>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
-            <h1>Showcase area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
+            <h1>Potatoes area for previous clients</h1>
         </section>
     )
 }
